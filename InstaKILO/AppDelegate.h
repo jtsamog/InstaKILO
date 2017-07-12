a091da3bd7477a0c7e8e4735bc470379331b5c65
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-12 16:19:33 -0400

    Initial Commit

---
+++ InstaKILO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKILO
+//
+//  Created by Endeavour2 on 7/12/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
